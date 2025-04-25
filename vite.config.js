985521aeeceead9ca6c1321c8fa7b6a56c80aa22
y <54428434+luissiko@users.noreply.github.com>
2025-04-25 06:17:59 -0500

    🚀 Corregido vite.config.js con base path

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
-  base: '/vite-race-game/', // 👈 Muy importante para GitHub Pages
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+  base: '/vite-race-game/', // 👈 muy importante para GitHub Pages
+  plugins: [react()]
 })
